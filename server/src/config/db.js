70d8c1f43dfbda6fa40c51881781640bf32cfcd6
Chitra Hemant Kulkarni <kulkarnichitra2002@gmail.com>
2026-07-23 20:46:25 +0530

    fix: correct MongoDB connection URI and improve logging

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () =>  {
-    try{
-        await mongoose.connect(process.evn.MONGODB_URI);
-        console.log("MongoDB Successfully Connected");
-    } catch (error) {
-         console.error("❌ MongoDB connection failed");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed");
     console.error(error.message);
     process.exit(1);
-    }
+  }
 };
 
-module.exports = connectDB
+module.exports = connectDB;
