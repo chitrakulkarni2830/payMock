8026176c435a74b5f92c1a61551ea5e15c3368e5
Chitra Hemant Kulkarni <kulkarnichitra2002@gmail.com>
2026-07-23 22:09:49 +0530

    feat: add payment creation controller

---
+++ server/src/controllers/paymentController.js
@@ -0,0 +1,32 @@
+const { nanoid } = require("nanoid");
+const Payment = require("../models/Payment");
+const { captureOwnerStack } = require("react");
+
+const createPayment = async (req,res) => {
+    try{
+    const { merchantName, customerName, amount, paymentMethod } = req.body;
+
+    const payment = await Payment.create ({
+        paymentId: nanoid(10),
+        merchantName,
+        customerName,
+        amount,
+        paymentMethod,
+    });
+
+    res.status(201).json({
+        success: true,
+        message: "Payment created successfully",
+        data: payment,
+    });
+  } catch (error) {
+        res.status(500).json ({
+            success: false,
+            message: error.message,
+        });
+  }
+};
+
+module.exports = {
+    createPayment,
+};
