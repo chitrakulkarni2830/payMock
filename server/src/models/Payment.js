79e106c889dcf30baa4f0a2a45aad19303498111
Chitra Hemant Kulkarni <kulkarnichitra2002@gmail.com>
2026-07-23 20:58:18 +0530

    feat: add Payment model

---
+++ server/src/models/Payment.js
@@ -0,0 +1,45 @@
+const mongoose = require ("mongoose");
+
+const paymentSchema = new mongoose.Schema (
+    {
+    paymentId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    merchantName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    customerName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    currency: {
+      type: String,
+      default: "INR",
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["UPI", "Card", "Net Banking", "Wallet"],
+      default: "UPI",
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Success", "Failed"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("Payment", paymentSchema);
